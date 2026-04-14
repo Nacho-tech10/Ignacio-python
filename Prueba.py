963ed556954721564322eec5288f40676d104f54
Nacho-tech10 <ncona13@gmail.com>
2026-04-14 16:34:02 -0400

    primer commit

---
+++ Prueba.py
@@ -0,0 +1,41 @@
+# print("Ingrese su nombre")
+# nombre=input()
+# edad=int(input("Ingrese su edad: "))
+
+# print("Hola", nombre, "Su edad es", edad)
+
+# if edad>=18:
+#     print("Es mayor de edad")
+# else:
+#     print("Es menor de edad")
+
+
+
+# Solicitar un numero al usuario y
+# Validar si un numero es positivo
+
+# num=int(input("Ingrese un numero: "))
+
+# if num>0:
+#     print("El numero es positivo")
+# else:
+#     print("El numero es negativo")
+
+
+
+# n1=float(input("Ingrese un numero: "))
+# n2=float(input("Ingrese otro numero: "))
+# n3=float(input("Ingrese otro numero: "))
+
+# prom=(n1+n2+n3)/3
+
+# print("El promedio es: ", round(prom, 1))
+
+# if prom>=4:
+#     print("El alumno aprovo")
+# else:
+#     print("El alumno reprovo")
+
+
+
+
