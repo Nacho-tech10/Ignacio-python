911e37ab04d4205d4d3238f76129e2dfca2aaa92
Nacho-tech10 <ncona13@gmail.com>
2026-04-14 17:11:31 -0400

    Segundo commit

---
+++ for.py
@@ -0,0 +1,35 @@
+# for i in range(6):
+#     print("Repeticion n",i+1)
+
+
+
+# Pida un numero al usuario y salude
+# esa cantidad de veces
+
+
+
+# num=int(input("Ponga un numero: "))
+# for i in range(num):
+#     print("Hola")
+
+
+
+
+
+# Pida un numero al usuario y obtenga
+# el promedio de todos esos numeros
+
+
+
+
+
+num=int(input("Ponga un numero: "))
+suma=0
+
+
+for i in range(num):
+    numero=int(input("Ingrese el numero"))
+    suma=suma+numero
+prom=suma/num
+print("EL promedio es: ", prom)
+
