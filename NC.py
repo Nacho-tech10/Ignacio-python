e78912b98c0e95753db3ce41f6288da73c76106a
Nacho-tech10 <ncona13@gmail.com>
2026-04-21 16:53:23 -0400

    Cuarto commit

---
+++ NC.py
@@ -0,0 +1,46 @@
+
+# n=" Carolina"
+# name="Alonso Robles"
+
+# print(name[0])
+# print(len(name))
+# print(n.strip())
+# print(name.lower())
+# print(name.upper())
+# print(name.replace("Robles", "Lopez"))
+# print(name.split())
+
+
+
+
+# con=input("Introduzca la contraseña: ")
+
+# if con=="SHAZAM":
+#     print("Contraseña correcta")
+# else:
+#     print("Contraseña incorrecta")
+ 
+
+
+
+
+
+# name=input("Porfavor introduzca un nombre de usuario: ")
+# if len(name)<4:
+#     print("ERROR, el usuario debe ser mayor de 4 caracteres")
+# elif len(name)>10:
+#     print("ERROR, el usuario debe ser menor de 10 caracteres")
+# else:
+#     print("Usuario creado correctamente")
+
+
+
+
+
+
+
+# pin=int(input("Ingrese su PIN: "))
+# if len(str(pin))==4:
+#     print("PIN correcto")
+# else:
+#     print("PIN erroneo")
