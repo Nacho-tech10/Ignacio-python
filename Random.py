0f394134a1fdd0d92b4657028dcc6fa1ed7acfc7
nacho-tech10 <ncona13@gmail.com>
2026-05-05 17:09:37 -0400

    Quinto commit

---
+++ Random.py
@@ -0,0 +1,45 @@
+# import random
+
+# num1=random.randint(1,9)
+# num2=random.randint(1,9)
+# num3=random.randint(1,9)
+# t1=False
+# t2=False
+# t3=False
+# nums=0
+# print(f"Los numeros generados son: {num1}, {num2}, {num3}")
+# while not t1 or not t2 or not t3:
+#     numerito=random.randint(1,9)
+#     print("El numero es", numerito)
+
+
+
+
+
+
+
+
+
+
+# na=0
+
+# peso=int(input("Cuanto pesa el producto? (en gramos): "))
+# if peso>0:
+#     sodio=int(input("Cual es el porcentaje de sodio?: "))
+#     if sodio>0 and sodio<=100:
+#         print("Presione 1 si es nacional")
+#         print("Presione 2 si es internaconal")
+#         input(na)
+
+#     else:
+#         print("Ponga bien el porcentaje")
+# else:
+#     print("Ponga bien el peso")
+
+
+
+
+
+
+
+
